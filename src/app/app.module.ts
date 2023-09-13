66bd94bfec309e69629d1f4181a7c617f9cb9a4a
Bucklan <as1kenjebaev@gmail.com>
2023-09-14 00:31:34 +0600

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {PostCreateComponent} from './posts/post-create/post-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-PostCreateComponent,
+    PostCreateComponent,
   ],
   imports: [
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
